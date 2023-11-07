6928b7eca87374edcbe0a5127986dd0d281ba2c0
nelson40514 <33085066+nelson40514@users.noreply.github.com>
2023-11-07 02:20:16 +0000

    Move to Replit

--- main.py
+++ main.py
@@ -82,8 +82,9 @@ def handle_message(event):
         else:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=reply_msg))
+                TextSendMessage(text=reply_msg)
+            )
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
